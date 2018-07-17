86b9467413ec236b045d93faa835c5a180c54002
Ravil <bilravil@ya.ru>
2018-07-18 01:16:05 +0300

    bug fixes

--- index.js
+++ index.js
@@ -84,25 +84,7 @@ function sendText(sender, name, price, image) {
                {
                 "title": name,
                 "image_url": image,
-                "subtitle": price,
-                "default_action": {
-                  "type": "web_url",
-                  "url": "https://petersfancybrownhats.com/view?item=103",
-                  "messenger_extensions": false,
-                  "webview_height_ratio": "tall",
-                  "fallback_url": "https://petersfancybrownhats.com/"
-                },
-                "buttons":[
-                  {
-                    "type":"web_url",
-                    "url":"https://petersfancybrownhats.com",
-                    "title":"View Website"
-                  },{
-                    "type":"postback",
-                    "title":"Start Chatting",
-                    "payload":"DEVELOPER_DEFINED_PAYLOAD"
-                  }              
-                ]      
+                "subtitle": price
               }
             ]
           }
