404dd700a1d442e47dbc9f4630a8c9b878e0c969
Ravil <bilravil@ya.ru>
2018-07-18 00:11:40 +0300

    bug fixes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const port = 3000;
+const port = 5000;
 const app = express()
 
 app.set('port', (port))
